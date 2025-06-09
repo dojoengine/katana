dd5629a2325d096d1e892f5fcfd858158def6a91
github-action-benchmark <github@users.noreply.github.com>
2025-06-09 04:13:08 +0000

    add Benchmark (cargo) benchmark result for 143addf988302736f1c68dbe3fe78ff82b95a4b6

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749197167626,
+  "lastUpdate": 1749442388516,
   "repoUrl": "https://github.com/dojoengine/katana",
   "entries": {
     "Benchmark": [
@@ -863,6 +863,60 @@ window.BENCHMARK_DATA = {
             "name": "Katana.Startup",
             "value": 124008566,
             "range": "± 338316",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tarrencev@users.noreply.github.com",
+            "name": "Tarrence van As",
+            "username": "tarrencev"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "143addf988302736f1c68dbe3fe78ff82b95a4b6",
+          "message": "feat: distributed tracing w/ gcloud trace  (#103)\n\nCo-authored-by: Ammar Arif <evergreenkary@gmail.com>",
+          "timestamp": "2025-06-09T11:53:10+08:00",
+          "tree_id": "517cbfe0503fbe83643499590331b24a8242c476",
+          "url": "https://github.com/dojoengine/katana/commit/143addf988302736f1c68dbe3fe78ff82b95a4b6"
+        },
+        "date": 1749442386826,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 3027760,
+            "range": "± 43116",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 393673,
+            "range": "± 7335",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 2877621066,
+            "range": "± 238708415",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 16243834,
+            "range": "± 239849",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Katana.Startup",
+            "value": 125883556,
+            "range": "± 558712",
             "unit": "ns/iter"
           }
         ]
