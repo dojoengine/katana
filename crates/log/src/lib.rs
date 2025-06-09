71564fc1c7b203af8e667cd688fc614d2839b746
Ammar Arif <evergreenkary@gmail.com>
2025-06-09 19:45:29 +0800

    wip

--- crates/log/src/lib.rs
+++ crates/log/src/lib.rs
@@ -1,6 +1,13 @@
+use std::fs::{File, OpenOptions};
+use std::io::stdout;
+use std::marker::PhantomData;
+use std::path::PathBuf;
+use std::sync::Arc;
+
 use opentelemetry_gcloud_trace::errors::GcloudTraceError;
 use tracing::subscriber::SetGlobalDefaultError;
 use tracing_log::log::SetLoggerError;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{filter, EnvFilter, Layer};
@@ -15,6 +22,210 @@ pub use fmt::LogFormat;
 pub enum TracerConfig {
     Gcloud(gcloud::GcloudConfig),
 }
+
+#[derive(Debug, Default, Clone)]
+pub enum LogOutput {
+    #[default]
+    Stdout,
+    File(PathBuf),
+}
+
+#[derive(Debug)]
+
+pub struct Logger {}
+
+trait LoggerOutput {
+    type Writer: for<'writer> MakeWriter<'writer> + 'static;
+    fn writer(&self) -> Self::Writer;
+}
+
+struct Stdout;
+
+struct FileOutput {
+    path: PathBuf,
+}
+
+impl LoggerOutput for Stdout {
+    type Writer = Arc<std::io::Stdout>;
+    fn writer(&self) -> Self::Writer {
+        Arc::new(std::io::stdout())
+    }
+}
+impl LoggerOutput for FileOutput {
+    type Writer = File;
+    fn writer(&self) -> Self::Writer {
+        OpenOptions::new().create(true).append(true).open(&self.path).unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub struct LoggerBuilder<W = Stdout> {
+    dev: bool,
+    format: LogFormat,
+    filter: Option<String>,
+    writer: W,
+    telemetry_config: Option<TracerConfig>,
+}
+
+impl LoggerBuilder {
+    pub fn new() -> Self {
+        Self {
+            dev: false,
+            filter: None,
+            writer: Stdout,
+            telemetry_config: None,
+            format: LogFormat::Full,
+        }
+    }
+}
+
+impl<W> LoggerBuilder<W> {
+    pub fn with_writer<W2: LoggerOutput>(self, writer: W2) -> LoggerBuilder<W2> {
+        LoggerBuilder {
+            writer,
+            dev: self.dev,
+            format: self.format,
+            filter: self.filter,
+            telemetry_config: self.telemetry_config,
+        }
+    }
+
+    pub fn file<P: Into<PathBuf>>(self, path: P) -> LoggerBuilder<FileOutput> {
+        LoggerBuilder {
+            dev: self.dev,
+            format: self.format,
+            filter: self.filter,
+            telemetry_config: self.telemetry_config,
+            writer: FileOutput { path: path.into() },
+        }
+    }
+
+    pub fn format(mut self, fmt: LogFormat) -> Self {
+        self.format = fmt;
+        self
+    }
+
+    pub fn dev(mut self, enabled: bool) -> Self {
+        self.dev = enabled;
+        self
+    }
+
+    pub fn telemetry(mut self, config: TracerConfig) -> Self {
+        self.telemetry_config = Some(config);
+        self
+    }
+
+    pub fn filter<S: Into<String>>(mut self, filter: S) -> Self {
+        self.filter = Some(filter.into());
+        self
+    }
+
+    // pub async fn init(self) -> Result<(), Error> {
+    //     let default_filter = if self.dev {
+    //         format!("{DEFAULT_LOG_FILTER},server=debug")
+    //     } else {
+    //         DEFAULT_LOG_FILTER.to_string()
+    //     };
+
+    //     let filter_str = self.filter.clone().unwrap_or(default_filter);
+    //     let filter = EnvFilter::try_from_default_env().or(EnvFilter::try_new(&filter_str))?;
+
+    //     match self.writer {
+    //         LogOutput::None => {
+    //             tracing_subscriber::registry().with(filter).init();
+    //             return Ok(());
+    //         }
+    //         LogOutput::Stdout => {
+    //             self.init_with_stdout_output(filter).await?;
+    //         }
+    //         LogOutput::File(ref path) => {
+    //             self.init_with_file_output(filter, path.to_path_buf()).await?;
+    //         }
+    //     }
+
+    //     Ok(())
+    // }
+
+    // async fn init_with_stdout_output(self, filter: EnvFilter) -> Result<(), Error> {
+    //     if let Some(telemetry_config) = self.telemetry_config {
+    //         let telemetry = match telemetry_config {
+    //             TracerConfig::Gcloud(cfg) => {
+    //                 let tracer = gcloud::init_gcp_tracer(&cfg).await?;
+    //                 tracing_opentelemetry::layer().with_tracer(tracer)
+    //             }
+    //         };
+
+    //         let fmt = match self.format {
+    //             LogFormat::Full => tracing_subscriber::fmt::layer().boxed(),
+    //             LogFormat::Json => {
+    //                 let a = tracing_subscriber::fmt::layer().json();
+    //                 a.boxed()
+    //             }
+    //         };
+
+    //         tracing_subscriber::registry().with(filter).with(telemetry).with(fmt).init();
+    //     } else {
+    //         let fmt = match self.format {
+    //             LogFormat::Full => tracing_subscriber::fmt::layer().boxed(),
+    //             LogFormat::Json => tracing_subscriber::fmt::layer().json().boxed(),
+    //         };
+
+    //         tracing_subscriber::registry().with(filter).with(fmt).init();
+    //     }
+
+    //     Ok(())
+    // }
+
+    // async fn init_with_file_output(self, filter: EnvFilter, path: PathBuf) -> Result<(), Error> {
+    //     let file = std::fs::OpenOptions::new()
+    //         .create(true)
+    //         .append(true)
+    //         .open(&path)
+    //         .map_err(|e| Error::FileOpen(path.clone(), e))?;
+
+    //     if let Some(telemetry_config) = self.telemetry_config {
+    //         let telemetry = match telemetry_config {
+    //             TracerConfig::Gcloud(cfg) => {
+    //                 let tracer = gcloud::init_gcp_tracer(&cfg).await?;
+    //                 tracing_opentelemetry::layer().with_tracer(tracer)
+    //             }
+    //         };
+
+    //         let fmt = match self.format {
+    //             LogFormat::Full => {
+    //                 tracing_subscriber::fmt::layer().with_writer(Arc::new(stdout())).boxed()
+    //             }
+    //             LogFormat::Json => {
+    //                 tracing_subscriber::fmt::layer().json().with_writer(file).boxed()
+    //             }
+    //         };
+
+    //         tracing_subscriber::registry().with(filter).with(telemetry).with(fmt).init();
+    //     } else {
+    //         let fmt = match self.format {
+    //             LogFormat::Full => tracing_subscriber::fmt::layer().with_writer(file).boxed(),
+    //             LogFormat::Json => {
+    //                 tracing_subscriber::fmt::layer().json().with_writer(file).boxed()
+    //             }
+    //         };
+
+    //         tracing_subscriber::registry().with(filter).with(fmt).init();
+    //     }
+
+    //     Ok(())
+    // }
+}
+
+impl<W: LoggerOutput> LoggerBuilder<W> {
+    pub fn build(self) {
+        todo!()
+    }
+}
+
+const DEFAULT_LOG_FILTER: &str =
+    "cairo_native::compiler=off,pipeline=debug,stage=debug,info,tasks=debug,executor=trace,\
+     forking::backend=trace,blockifier=off,jsonrpsee_server=off,hyper=off,messaging=debug,\
+     node=error,explorer=info,jsonrpsee_core::middleware::layer::logger=trace";
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -33,6 +244,9 @@ pub enum Error {
     #[error("failed to install crypto provider")]
     InstallCryptoFailed,
 
+    #[error("failed to open file {0}: {1}")]
+    FileOpen(PathBuf, std::io::Error),
+
     #[error(transparent)]
     OtelSdk(#[from] opentelemetry_sdk::error::OTelSdkError),
 }
@@ -42,46 +256,11 @@ pub async fn init(
     dev_log: bool,
     telemetry_config: Option<TracerConfig>,
 ) -> Result<(), Error> {
-    const DEFAULT_LOG_FILTER: &str =
-        "cairo_native::compiler=off,pipeline=debug,stage=debug,info,tasks=debug,executor=trace,\
-         forking::backend=trace,blockifier=off,jsonrpsee_server=off,hyper=off,messaging=debug,\
-         node=error,explorer=info,jsonrpsee_core::middleware::layer::logger=trace";
+    let mut builder = LoggerBuilder::new().format(format).dev(dev_log);
 
-    let filter = if dev_log {
-        format!("{DEFAULT_LOG_FILTER},server=debug")
-    } else {
-        DEFAULT_LOG_FILTER.to_string()
-    };
-
-    // If the user has set the `RUST_LOG` environment variable, then we prioritize it.
-    // Otherwise, we use the default log filter.
-    // TODO: change env var to `KATANA_LOG`.
-    let filter = EnvFilter::try_from_default_env().or(EnvFilter::try_new(&filter))?;
-
-    // Initialize tracing subscriber with optional telemetry
-    if let Some(telemetry_config) = telemetry_config {
-        // Initialize telemetry layer based on exporter type
-        let telemetry = match telemetry_config {
-            TracerConfig::Gcloud(cfg) => {
-                let tracer = gcloud::init_gcp_tracer(&cfg).await?;
-                tracing_opentelemetry::layer().with_tracer(tracer)
-            }
-        };
-
-        let fmt = match format {
-            LogFormat::Full => tracing_subscriber::fmt::layer().boxed(),
-            LogFormat::Json => tracing_subscriber::fmt::layer().json().boxed(),
-        };
-
-        tracing_subscriber::registry().with(filter).with(telemetry).with(fmt).init();
-    } else {
-        let fmt = match format {
-            LogFormat::Full => tracing_subscriber::fmt::layer().boxed(),
-            LogFormat::Json => tracing_subscriber::fmt::layer().json().boxed(),
-        };
-
-        tracing_subscriber::registry().with(filter).with(fmt).init();
+    if let Some(config) = telemetry_config {
+        builder = builder.telemetry(config);
     }
 
-    Ok(())
+    builder.init().await
 }
