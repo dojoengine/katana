f2100b8b7f1a2760e5c1a65e4ab766adf6119438
Ammar Arif <evergreenkary@gmail.com>
2025-07-10 10:55:38 +0800

    add doc comments

--- crates/primitives/src/version.rs
+++ crates/primitives/src/version.rs
@@ -1,4 +1,14 @@
 /// The currently supported version of the Starknet protocol.
+///
+/// ## IMPORTANT
+///
+/// This version must correspond to the minimum Cairo/Sierra version we want to support. Ideally,
+/// this version should be synchronized with Dojo's Cairo version.
+///
+/// As of Cairo v2.10.0, contracts written with that version are only deployable on Starknet
+/// >=0.13.4. Check out the [release notes] for more info.
+///
+/// [release notes]: https://community.starknet.io/t/cairo-v2-10-0-is-out/115362
 pub const CURRENT_STARKNET_VERSION: StarknetVersion = StarknetVersion::new([0, 13, 4, 0]);
 
 /// Starknet protocol version.
