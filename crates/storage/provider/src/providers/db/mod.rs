7152f64196d30c905fec7da067a57569f3086fcb
Ammar Arif <evergreenkary@gmail.com>
2025-07-11 00:55:13 +0800

    check all classes are present

--- crates/storage/provider/src/providers/db/mod.rs
+++ crates/storage/provider/src/providers/db/mod.rs
@@ -712,22 +712,29 @@ impl<Db: Database> BlockWriter for DbProvider<Db> {
             }
 
             // insert classes
+            let mut classes_registry = states.classes;
 
             for (class_hash, compiled_hash) in states.state_updates.declared_classes {
                 db_tx.put::<tables::CompiledClassHashes>(class_hash, compiled_hash)?;
 
                 db_tx.put::<tables::ClassDeclarationBlock>(class_hash, block_number)?;
-                db_tx.put::<tables::ClassDeclarations>(block_number, class_hash)?
+                db_tx.put::<tables::ClassDeclarations>(block_number, class_hash)?;
+
+                let entry = classes_registry.remove(&class_hash);
+                let class = entry.ok_or(ProviderError::MissingContractClass(class_hash))?;
+                db_tx.put::<tables::Classes>(class_hash, class)?;
             }
 
             for class_hash in states.state_updates.deprecated_declared_classes {
                 db_tx.put::<tables::ClassDeclarationBlock>(class_hash, block_number)?;
-                db_tx.put::<tables::ClassDeclarations>(block_number, class_hash)?
-            }
+                db_tx.put::<tables::ClassDeclarations>(block_number, class_hash)?;
 
-            for (class_hash, class) in states.classes {
+                let entry = classes_registry.remove(&class_hash);
+                let class = entry.ok_or(ProviderError::MissingContractClass(class_hash))?;
                 db_tx.put::<tables::Classes>(class_hash, class)?;
             }
+
+            assert!(classes_registry.is_empty(), "all declared classes should've been stored");
 
             // insert storage changes
             {
