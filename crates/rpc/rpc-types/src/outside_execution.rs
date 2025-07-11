cbd1621d4637b40c7b283eef092cee61637a6333
Ammar Arif <evergreenkary@gmail.com>
2025-07-10 21:11:06 -0400

    fix(rpc-types): de/serialize as hex string (#177)

--- crates/rpc/rpc-types/src/outside_execution.rs
+++ crates/rpc/rpc-types/src/outside_execution.rs
@@ -6,6 +6,7 @@
 //!
 //! Based on [SNIP-9](https://github.com/starknet-io/SNIPs/blob/main/SNIPS/snip-9.md).
 
+use cainome::cairo_serde::{deserialize_from_hex, serialize_as_hex};
 use cainome::cairo_serde_derive::CairoSerde;
 use katana_primitives::{ContractAddress, Felt};
 use serde::{Deserialize, Serialize};
@@ -21,6 +22,13 @@ pub struct Call {
     pub calldata: Vec<Felt>,
 }
 
+/// Nonce channel
+#[derive(Clone, CairoSerde, PartialEq, Debug, Serialize, Deserialize)]
+pub struct NonceChannel(
+    Felt,
+    #[serde(serialize_with = "serialize_as_hex", deserialize_with = "deserialize_from_hex")] u128,
+);
+
 /// Outside execution version 2 (SNIP-9 standard).
 #[derive(Clone, CairoSerde, Serialize, Deserialize, PartialEq, Debug)]
 pub struct OutsideExecutionV2 {
@@ -29,8 +37,10 @@ pub struct OutsideExecutionV2 {
     /// Unique nonce to prevent signature reuse.
     pub nonce: Felt,
     /// Timestamp after which execution is valid.
+    #[serde(serialize_with = "serialize_as_hex", deserialize_with = "deserialize_from_hex")]
     pub execute_after: u64,
     /// Timestamp before which execution is valid.
+    #[serde(serialize_with = "serialize_as_hex", deserialize_with = "deserialize_from_hex")]
     pub execute_before: u64,
     /// Calls to execute in order.
     pub calls: Vec<Call>,
@@ -41,11 +51,13 @@ pub struct OutsideExecutionV2 {
 pub struct OutsideExecutionV3 {
     /// Address allowed to initiate execution ('ANY_CALLER' for unrestricted).
     pub caller: ContractAddress,
-    /// Nonce with channel (nonce, channel).
-    pub nonce: (Felt, u128),
+    /// Nonce.
+    pub nonce: NonceChannel,
     /// Timestamp after which execution is valid.
+    #[serde(serialize_with = "serialize_as_hex", deserialize_with = "deserialize_from_hex")]
     pub execute_after: u64,
     /// Timestamp before which execution is valid.
+    #[serde(serialize_with = "serialize_as_hex", deserialize_with = "deserialize_from_hex")]
     pub execute_before: u64,
     /// Calls to execute in order.
     pub calls: Vec<Call>,
@@ -66,5 +78,148 @@ impl OutsideExecution {
             OutsideExecution::V2(v2) => v2.caller,
             OutsideExecution::V3(v3) => v3.caller,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use katana_primitives::{address, felt, ContractAddress, Felt};
+    use serde_json::json;
+    use starknet::macros::selector;
+
+    use crate::outside_execution::{Call, NonceChannel, OutsideExecutionV2, OutsideExecutionV3};
+
+    #[test]
+    fn outside_execution_v2_serialization() {
+        let outside_execution = OutsideExecutionV2 {
+            caller: address!("0x414e595f43414c4c4552"),
+            execute_after: 0,
+            execute_before: 3000000000,
+            calls: vec![
+                Call {
+                    to: address!(
+                        "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+                    ),
+                    selector: selector!("approve"),
+                    calldata: vec![
+                        felt!("0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4"),
+                        felt!("0x11c37937e08000"),
+                        Felt::ZERO,
+                    ],
+                },
+                Call {
+                    to: address!(
+                        "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+                    ),
+                    selector: selector!("transfer"),
+                    calldata: vec![
+                        felt!("0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4"),
+                        felt!("0x11c37937e08000"),
+                        Felt::ZERO,
+                    ],
+                },
+            ],
+            nonce: felt!("0x564b73282b2fb5f201cf2070bf0ca2526871cb7daa06e0e805521ef5d907b33"),
+        };
+
+        let serialized = serde_json::to_value(outside_execution).unwrap();
+
+        let expected = json!({
+            "caller": "0x414e595f43414c4c4552",
+            "execute_after": "0x0",
+            "execute_before": "0xb2d05e00",
+            "calls": [
+                {
+                    "to": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                    "selector": "0x219209e083275171774dab1df80982e9df2096516f06319c5c6d71ae0a8480c",
+                    "calldata": [
+                        "0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4",
+                        "0x11c37937e08000",
+                        "0x0"
+                    ]
+                },
+                {
+                    "to": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                    "selector": "0x83afd3f4caedc6eebf44246fe54e38c95e3179a5ec9ea81740eca5b482d12e",
+                    "calldata": [
+                        "0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4",
+                        "0x11c37937e08000",
+                        "0x0"
+                    ]
+                }
+            ],
+            "nonce": "0x564b73282b2fb5f201cf2070bf0ca2526871cb7daa06e0e805521ef5d907b33",
+        });
+
+        similar_asserts::assert_eq!(serialized, expected);
+    }
+
+    #[test]
+    fn outside_execution_v3_serialization() {
+        let outside_execution = OutsideExecutionV3 {
+            caller: address!("0x414e595f43414c4c4552"),
+            execute_after: 0,
+            execute_before: 3000000000,
+            calls: vec![
+                Call {
+                    to: address!(
+                        "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+                    ),
+                    selector: selector!("approve"),
+                    calldata: vec![
+                        felt!("0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4"),
+                        felt!("0x11c37937e08000"),
+                        Felt::ZERO,
+                    ],
+                },
+                Call {
+                    to: address!(
+                        "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+                    ),
+                    selector: selector!("transfer"),
+                    calldata: vec![
+                        felt!("0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4"),
+                        felt!("0x11c37937e08000"),
+                        Felt::ZERO,
+                    ],
+                },
+            ],
+            nonce: NonceChannel(
+                felt!("0x564b73282b2fb5f201cf2070bf0ca2526871cb7daa06e0e805521ef5d907b33"),
+                10,
+            ),
+        };
+
+        let serialized = serde_json::to_value(outside_execution).unwrap();
+
+        let expected = json!({
+            "caller": "0x414e595f43414c4c4552",
+            "execute_after": "0x0",
+            "execute_before": "0xb2d05e00",
+            "calls": [
+                {
+                    "to": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                    "selector": "0x219209e083275171774dab1df80982e9df2096516f06319c5c6d71ae0a8480c",
+                    "calldata": [
+                        "0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4",
+                        "0x11c37937e08000",
+                        "0x0"
+                    ]
+                },
+                {
+                    "to": "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                    "selector": "0x83afd3f4caedc6eebf44246fe54e38c95e3179a5ec9ea81740eca5b482d12e",
+                    "calldata": [
+                        "0x50302d9f4df7a96567423f64f1271ef07537469d8e8c4dd2409cf3cc4274de4",
+                        "0x11c37937e08000",
+                        "0x0"
+                    ]
+                }
+            ],
+            "nonce": ["0x564b73282b2fb5f201cf2070bf0ca2526871cb7daa06e0e805521ef5d907b33", "0xa"],
+        });
+
+        similar_asserts::assert_eq!(serialized, expected);
     }
 }
