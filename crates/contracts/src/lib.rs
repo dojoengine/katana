088d97de5f651935e1bc9112e4f10152d2efc646
Ammar Arif <evergreenkary@gmail.com>
2025-07-29 23:31:26 -0400

    refactor(contracts): append cargo manifest dir path

--- crates/contracts/src/lib.rs
+++ crates/contracts/src/lib.rs
@@ -1,8 +1,8 @@
 pub mod contracts {
     use katana_contracts_macro::contract;
 
-    contract!(LegacyERC20, "crates/contracts/build/legacy/erc20.json");
-    contract!(GenesisAccount, "crates/contracts/build/legacy/account.json");
-    contract!(UniversalDeployer, "crates/contracts/build/legacy/universal_deployer.json");
-    contract!(Account, "crates/contracts/build/katana_account_Account.contract_class.json");
+    contract!(LegacyERC20, "{CARGO_MANIFEST_DIR}/build/legacy/erc20.json");
+    contract!(GenesisAccount, "{CARGO_MANIFEST_DIR}/build/legacy/account.json");
+    contract!(UniversalDeployer, "{CARGO_MANIFEST_DIR}/build/legacy/universal_deployer.json");
+    contract!(Account, "{CARGO_MANIFEST_DIR}/build/katana_account_Account.contract_class.json");
 }
