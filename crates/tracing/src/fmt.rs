716139b5ef3355c31594d778f484dae6f519d301
Ammar Arif <evergreenkary@gmail.com>
2025-08-24 21:19:03 -0400

    feat(tracing): format timestamp

--- crates/tracing/src/fmt.rs
+++ crates/tracing/src/fmt.rs
@@ -1,6 +1,8 @@
 use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
+use tracing_subscriber::fmt::format::Writer;
+use tracing_subscriber::fmt::time::{self};
 
 /// Format for logging output.
 #[derive(Debug, Copy, Clone, PartialEq, Deserialize, Serialize, Default)]
@@ -31,5 +33,36 @@ impl clap::ValueEnum for LogFormat {
             Self::Json => Some(clap::builder::PossibleValue::new("json")),
             Self::Full => Some(clap::builder::PossibleValue::new("full")),
         }
+    }
+}
+
+const DEFAULT_TIMESTAMP_FORMAT: &str = "%Y-%m-%d %H:%M:%S%.3f %Z";
+
+/// Default formatter for span and event timestamps.
+///
+/// Formats timestamps in local time with the following format:
+///
+/// ```console
+/// %Y-%m-%d %H:%M:%S%.3f %Z
+/// ```
+///
+/// Example output
+///
+/// ```console
+/// 2025-08-24 20:49:32.487 -04:00
+/// ```
+#[derive(Debug, Clone, Default)]
+pub struct LocalTime;
+
+impl LocalTime {
+    pub fn new() -> Self {
+        LocalTime
+    }
+}
+
+impl time::FormatTime for LocalTime {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
+        let time = chrono::Local::now();
+        write!(w, "{}", time.format(DEFAULT_TIMESTAMP_FORMAT))
     }
 }
