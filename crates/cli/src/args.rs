34dfd8be381a8153740b5d741b9f45471531d4a1
Ammar Arif <evergreenkary@gmail.com>
2025-09-10 13:41:33 -0400

    save

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -27,6 +27,7 @@ use katana_node::config::Config;
 use katana_node::Node;
 use katana_primitives::genesis::allocation::DevAllocationsGenerator;
 use katana_primitives::genesis::constant::DEFAULT_PREFUNDED_ACCOUNT_BALANCE;
+use katana_tracing::LogFormat;
 use serde::{Deserialize, Serialize};
 use tracing::info;
 use url::Url;
@@ -129,7 +130,17 @@ impl NodeArgs {
     pub async fn execute(&self) -> Result<()> {
         // Initialize logging with tracer
         let tracer_config = self.tracer_config();
-        katana_tracing::init(self.logging.log_format, tracer_config).await?;
+
+        match self.logging.log_format {
+            LogFormat::Full => {
+                katana_tracing::TracingBuilder::new().build()?;
+            }
+            LogFormat::Json => {
+                katana_tracing::TracingBuilder::new().json().init(tracer_config)?;
+            }
+        }
+
+        // katana_tracing::init(self.logging.log_format, tracer_config).await?;
         self.start_node().await
     }
 
