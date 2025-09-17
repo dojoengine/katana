9332b9ee8fd456ca2a9512e672911fa4de2bbb1f
Ammar Arif <evergreenkary@gmail.com>
2025-09-17 14:15:40 -0400

    remove feeder gateway client

--- crates/feeder-gateway/src/lib.rs
+++ crates/feeder-gateway/src/lib.rs
@@ -1,6 +1,4 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-// Allow these deps as they're used by the feeder-cli binary
-#![allow(unused_crate_dependencies)]
 
 pub mod client;
 pub mod types;
