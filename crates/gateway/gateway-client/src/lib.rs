81dd17d04f19628aad4cd60251b92d3bc62db802
Ammar Arif <evergreenkary@gmail.com>
2025-10-22 17:12:00 -0400

    wip

--- crates/gateway/gateway-client/src/lib.rs
+++ crates/gateway/gateway-client/src/lib.rs
@@ -208,8 +208,8 @@ impl Client {
 // </head>
 // <body text=#000000 bgcolor=#ffffff>
 // <h1>Error: Server Error</h1>
-// <h2>The server encountered a temporary error and could not complete your request.<p>Please try again in 30 seconds.</h2>
-// <h2></h2>
+// <h2>The server encountered a temporary error and could not complete your request.<p>Please try
+// again in 30 seconds.</h2> <h2></h2>
 // </body></html>
 
 #[derive(Debug, thiserror::Error)]
