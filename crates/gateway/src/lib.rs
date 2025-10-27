296f1afeaab13b51dce93a1571947629894584d0
Ammar Arif <evergreenkary@gmail.com>
2025-10-27 18:06:41 -0400

    fmt

--- crates/gateway/src/lib.rs
+++ crates/gateway/src/lib.rs
@@ -1,10 +1,9 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-#[cfg(not(test))]
-use {anyhow as _, clap as _, serde_json as _, tracing as _};
-
 #[cfg(all(not(test), feature = "server"))]
 use tokio as _;
+#[cfg(not(test))]
+use {anyhow as _, clap as _, serde_json as _, tracing as _};
 
 pub mod client;
 #[cfg(feature = "server")]
