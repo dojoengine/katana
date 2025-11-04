c9e4a4929b0e81f9a47f2fc7c2d874b34df2b69e
Remy Baranx <remy.baranx@gmail.com>
2025-11-04 10:31:26 -0700

    add VRF management in outside_execution function

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -15,6 +15,8 @@ use cartridge::paymaster::layer::PaymasterLayer;
 use cartridge::paymaster::Paymaster;
 #[cfg(feature = "cartridge")]
 use cartridge::rpc::{CartridgeApi, CartridgeApiServer};
+#[cfg(feature = "cartridge")]
+use cartridge::vrf::{VrfContext, CARTRIDGE_VRF_DEFAULT_PRIVATE_KEY};
 use config::rpc::RpcModuleKind;
 use config::Config;
 use http::header::CONTENT_TYPE;
@@ -326,6 +328,8 @@ impl Node {
                 config.chain.id(),
                 *pm_address,
                 SigningKey::from_secret_scalar(pm_private_key),
+                // TODO RBA: should we get the private key from the config?
+                VrfContext::new(CARTRIDGE_VRF_DEFAULT_PRIVATE_KEY, *pm_address),
             ))
         } else {
             None
