c71063ca15271691029b6f5f7cd6dc92dceaa4a1
Ammar Arif <evergreenkary@gmail.com>
2025-12-08 13:56:00 -0500

    chore(trie): add comments

--- crates/trie/src/lib.rs
+++ crates/trie/src/lib.rs
@@ -40,14 +40,23 @@ where
 {
     pub fn new(db: DB) -> Self {
         let config = BonsaiStorageConfig {
-            // we have our own implementation of storing trie changes
-            max_saved_trie_logs: Some(64),
+            // This field controls what's the oldest block we can revert to.
+            //
+            // The value 5 is chosen arbitrarily as a placeholder. This value should be
+            // configurable.
+            max_saved_trie_logs: Some(5),
+
             // in the bonsai-trie crate, this field seems to be only used in rocksdb impl.
             // i dont understand why would they add a config thats implementation specific ????
             //
             // this config should be used by our implementation of the
             // BonsaiPersistentDatabase::snapshot()
+            //
+            // note: currently, this value is not being used for anything. our trie will stores
+            // all created snapshots.
             max_saved_snapshots: Some(64usize),
+
+            // creates a snapshot for every block
             snapshot_interval: 1,
         };
 
