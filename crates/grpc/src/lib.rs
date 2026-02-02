a827be6ece27cd67149ded255216b41dbcb59bf4
Ammar Arif <kariy@users.noreply.github.com>
2026-02-02 03:05:26 +0000

    refactor(grpc): remove unused config

--- crates/grpc/src/lib.rs
+++ crates/grpc/src/lib.rs
@@ -5,14 +5,12 @@
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-mod config;
 mod error;
 mod handlers;
 mod protos;
 mod server;
 
 // Re-export conversion module for internal use
-pub use config::GrpcConfig;
 pub use handlers::StarknetService;
 pub use protos::starknet::starknet_server::StarknetServer;
 pub use protos::starknet::starknet_trace_server::StarknetTraceServer;
