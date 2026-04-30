194ef75027a308f99218167a2ae8044ac609ac55
Ammar Arif <evergreenkary@gmail.com>
2026-04-29 20:26:55 -0500

    re-export rpc types

--- crates/starknet/src/rpc.rs
+++ crates/starknet/src/rpc.rs
@@ -1,6 +1,6 @@
 use jsonrpsee::core::client;
 use jsonrpsee::http_client::HttpClient;
-use katana_primitives::block::{BlockIdOrTag, ConfirmedBlockIdOrTag};
+use katana_primitives::block::ConfirmedBlockIdOrTag;
 use katana_primitives::class::ClassHash;
 use katana_primitives::contract::{Nonce, StorageKey};
 use katana_primitives::transaction::TxHash;
@@ -27,10 +27,13 @@ use katana_rpc_types::trace::{
 use katana_rpc_types::transaction::RpcTxWithHash;
 use katana_rpc_types::trie::{ContractStorageKeys, GetStorageProofResponse};
 use katana_rpc_types::{
-    CallResponse, EstimateFeeSimulationFlag, EventFilter, FeeEstimate, FunctionCall,
-    ResultPageRequest, SimulationFlag, SyncingResponse, TxStatus,
+    CallResponse, EstimateFeeSimulationFlag, EventFilter, FeeEstimate, ResultPageRequest,
+    SimulationFlag, SyncingResponse, TxStatus,
 };
+
 use url::Url;
+
+pub use katana_rpc_types::{BlockIdOrTag, FunctionCall};
 
 type Result<T> = std::result::Result<T, StarknetRpcClientError>;
 
